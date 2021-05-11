37cf4a90b64e94c80582b7ef195317de74e1a1d8
ShubhamTyagi <styagi.2301@gmail.com>
2021-05-12 00:03:32 +0530

    Created puzzle board manually

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite picture;
+
+
+    public static GameManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Vector2 actualPosition;
+    public Vector2 actualScale = Vector2.one;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        actualPosition = transform.localPosition;
+        gameObject.name = $"Piece_{transform.GetSiblingIndex()}";
+        spriteRenderer.sprite = GameManager.Instance.picture;
+        spriteRenderer.transform.localScale = actualScale;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+
+}
